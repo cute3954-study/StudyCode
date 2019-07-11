f317fe227bb5452603f681ce3eccbe148ef6301e
cute3 <cute3@MSI>
2019-07-11 14:55:04 +0900

    7/11 会員登録　追加

--- homepage_practice/register/db/registerMember.php
+++ homepage_practice/register/db/registerMember.php
@@ -1,14 +1,7 @@
 <?php
-$mysql_hostname = 'localhost';
-$mysql_username = 'root';
-$mysql_password = '123456';
-$mysql_database = 'homepagepra';
-$mysql_port = '3306';
-
-$mysqli = new mysqli($mysql_hostname, $mysql_username, $mysql_password, $mysql_database, $mysql_port);
-
+include $_SERVER['DOCUMENT_ROOT'] . '/homepage_practice/db/db.php';
 $userId = $_POST['userId'];
-$userPwd = md5($_POST['userPwd']);
+$userPwd = $_POST['userPwd1'];
 $userName = $_POST['userName'];
 $userYear = $_POST['userYear'];
 $userMonth = $_POST['userMonth'];
@@ -19,14 +12,27 @@ $userAdd1 = $_POST['userAdd1'];
 $userAdd2 = $_POST['userAdd2'];
 $userAdd3 = $_POST['userAdd3'];
 $userAdd4 = $_POST['userAdd4'];
-$userBS = $_POST['userBS'];
 
-$sql = "insert into userInfo (userId, userPwd, userName, userYear, userMonth, userDay, userEmail, userPost, userAdd1, userAdd2, userAdd3, userAdd4, userBS)";
-$sql = $sql . " value('$userId', '$userPwd', '$userName', '$userYear', '$userMonth', '$userDay', '$userEmail', '$userPost', '$userAdd1', '$userAdd2', '$userAdd3', '$userAdd4', '$userBS')";
-
-if ($mysqli->query($sql)) {
-    echo 'success inserting';
-}else{
-    echo 'fail to insert sql';
+$sql = "insert into userInfo (userId, userPwd, userName, userYear, userMonth, userDay, userEmail, userPost, userAdd1, userAdd2, userAdd3, userAdd4) values('$userId','$userPwd','$userName','$userYear','$userMonth','$userDay','$userEmail','$userPost','$userAdd1','$userAdd2','$userAdd3','$userAdd4')";
+$result = $db->query($sql);
+try {
+    if ($result) {
+        ?>
+<script>
+alert('<?php echo $userId?>さん、新規登録が完了しました！<br/>ログインお願いします。');
+location.href='/homepage_practice/index.php';
+</script>
+<?php
+    } else {
+        throw new Exception("新規登録が失敗しました。");
+    }
+} catch (Exception $e) {
+    $msg = $e->getMessage();
+?>
+	<script>
+		alert(<?php echo $msg?>);
+		history.back();
+	</script>
+<?php
 }
 ?>
